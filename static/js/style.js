caa8f53a817b99ccac739f617756add1d5726895
Groot1895 <kponnivalavan95@gmail.com>
2021-04-04 21:24:44 +0530

    demo

--- static/js/style.js
+++ static/js/style.js
@@ -70,3 +70,23 @@ function wetricem() {
 function gt() {
     document.getElementById("t").value = (parseFloat(document.getElementById("wt").value) + parseFloat(document.getElementById("rt").value) + parseFloat(document.getElementById("wrt").value)).toFixed(2);
 }
+
+// table sort
+function tsort() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
